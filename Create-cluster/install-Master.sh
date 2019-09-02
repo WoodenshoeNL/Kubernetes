cbb75db06d38f82e7620b0b096fe3d6519434b38
Michel Klomp <michel.klomp@sentia.com>
2019-09-02 07:58:28 +0200

    add flannel networking

--- Create-cluster/install-Master.sh
+++ Create-cluster/install-Master.sh
@@ -2,8 +2,8 @@
 
 
 #on the master, download the yaml files for the pod network
-wget https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
-wget https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+#wget https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+#wget https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
 
 
 #Create our kubernetes cluster, specifying a pod network range matching that in calico.yaml!
@@ -15,6 +15,8 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 #Download yaml files for your pod network
-kubectl apply -f rbac-kdd.yaml
-kubectl apply -f calico.yaml
+#kubectl apply -f rbac-kdd.yaml
+#kubectl apply -f calico.yaml
 
+#install flannel
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
