114503e3c7ddab7ac7f00bd92ac22ab538b4070a
Michel Klomp <michel.klomp@sentia.com>
2019-09-08 14:19:06 +0200

    add remove pvc

--- deploy-smilr/remove-smilr.sh
+++ deploy-smilr/remove-smilr.sh
@@ -7,4 +7,5 @@ kubectl delete -f data-api.svc.yaml
 kubectl delete -f frontend.svc.yaml
 kubectl delete -f frontend.deploy.yaml
 kubectl delete -f mongo.stateful.yaml
+kubectl delete pvc mongo-vol-mongodb-0
 kubectl delete -f mongo.PersistentVolume.yaml
